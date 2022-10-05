4acf9db9335361aebac4bc715d4c5b12c28f8607
Diogo Santos <diogo.angelo@designa.com.br>
2022-10-05 19:54:52 +0100

    New user form

--- src/app/pages/home/new-user/new-user.component.ts
+++ src/app/pages/home/new-user/new-user.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(4)]],
+    });
   }
 
+  public submit(): void {
+    console.log(this.form.value);
+  }
 }
